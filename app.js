cca10d4a646f4d887d74973aeba036727e7f4b6d
Dmitriy Dulin <dmitriy.dulin@namecheap.com>
2017-12-14 08:36:07 +0200

    Added basic environment for development

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// Log requests to the console.
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('*', (req, res) => {
+  res.status(200).send({
+    message: 'Welcome to the Advanced CRUD application.'
+  })
+})
+
+module.exports = app
