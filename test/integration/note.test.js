7ebb4c2ae7fb5d5bde922157c79a7128ebf31155
Dmitriy Dulin <dmitriy.dulin@namecheap.com>
2017-12-28 09:56:11 +0200

    Updated tests approach and readme file

--- test/integration/note.test.js
+++ test/integration/note.test.js
@@ -4,28 +4,30 @@ const app = require('../../app')
 
 const expect = chai.expect
 
-describe('Testing entry point GET /', () => {
-  it('Should response with welcome message', done => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect(response => {
-        expect(response.body).to.be.instanceof(Object)
-        expect(response.body.message).to.equal('Welcome to the Advanced CRUD application.')
-      })
-      .end(done)
+describe('Testing note routes', () => {
+  describe('Testing entry point GET /', () => {
+    it('Should response with welcome message', done => {
+      request(app)
+        .get('/')
+        .expect(200)
+        .expect(response => {
+          expect(response.body).to.be.instanceof(Object)
+          expect(response.body.message).to.equal('Welcome to the Advanced CRUD application.')
+        })
+        .end(done)
+    })
   })
-})
 
-describe('Testing entry point for API GET /api', () => {
-  it('Should response with welcome message to the API', done => {
-    request(app)
-      .get('/api')
-      .expect(200)
-      .expect(response => {
-        expect(response.body).to.be.instanceof(Object)
-        expect(response.body.message).to.equal('Welcome to the Notes API!')
-      })
-      .end(done)
+  describe('Testing entry point GET /api', () => {
+    it('Should response with welcome message to the API', done => {
+      request(app)
+        .get('/api')
+        .expect(200)
+        .expect(response => {
+          expect(response.body).to.be.instanceof(Object)
+          expect(response.body.message).to.equal('Welcome to the Notes API!')
+        })
+        .end(done)
+    })
   })
 })
